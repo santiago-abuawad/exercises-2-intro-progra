391cf8626bc82178bcdab354fdec134741979b05
Santiago Abuawad <85538772+santiago-abuawad@users.noreply.github.com>
2024-04-15 08:03:50 -0400

    x

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -49,7 +49,25 @@ void exercise_3(string s1) {
 }
 
 void exercise_4(int n) {
-  // TODO: YOUR CODE HERE
+  if (n == 0){
+    return 1;
+  }
+  if(n == 1){
+    return 1;
+  }
+  if(n < 0){
+    cout << "El número es negativo. Intentelo de nuevo\n";
+    return -1;
+  }
+  if(n > 14) {
+      cout << "El número es muy grande. Intentelo de nuevo\n";
+      return -1;
+  }
+  int resultado = 1;
+  for(int i = 2; i <= n; ++i) {
+    resultado *= i;
+  }
+  return resultado;
 }
 
 void exercise_5(int n, int k) {
