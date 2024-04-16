c23e176666940bc22874d39edd4a16e70de3c345
Santiago Abuawad <85538772+santiago-abuawad@users.noreply.github.com>
2024-04-15 22:34:01 -0400

    x

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -190,7 +190,7 @@ void exercise_17(int n) {
 
 void exercise_18_19(int debut, int fin) {
      if (debut < 0) {
-        return 0;
+      cout << "";
     } else {
     for(int i = debut; i <= fin ; i++) {
       int num = i;
